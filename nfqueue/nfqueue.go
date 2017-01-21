9675ee5e1c1b4469d0065d08097a05c5cf1ffeb4
Eliezer Croitoru <eliezer@ngtech.co.il>
2017-01-21 18:47:47 +0200

    Trying to fix something

--- nfqueue/nfqueue.go
+++ nfqueue/nfqueue.go
@@ -254,6 +254,20 @@ func (p *Payload) SetVerdict(verdict int) error {
     return nil
 }
 
+// SetVerdictMark issues a verdict for a packet, but a mark can be set
+//
+// Every queued packet _must_ have a verdict specified by userspace.
+func (p *Payload) SetVerdictMark(verdict int, mark uint32) error {
+    log.Printf("Setting verdict for packet %d: %d mark %lx\n",p.Id,verdict,mark)
+    C.nfq_set_verdict2(
+        p.c_qh,
+        C.u_int32_t(p.Id),
+        C.u_int32_t(verdict),
+        C.u_int32_t(mark),
+        0,nil)
+    return nil
+}
+
 // SetVerdictModified issues a verdict for a packet, but replaces the packet
 // with the provided one.
 //
